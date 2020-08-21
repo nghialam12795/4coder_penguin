39b497990b14208da2a47eb2a50d7694f81f35e5
Nghia Lam <nghialam12795@gmail.com>
2020-08-21 08:59:17 +0700

    Add gruvbox theme and code base structure

---
+++ script/build_win.bat
@@ -0,0 +1,26 @@
+:: A file written by:
+::  _______          .__    .__         .____                    
+::  \      \    ____ |  |__ |__|____    |    |   _____    _____  
+::  /   |   \  / ___\|  |  \|  \__  \   |    |   \__  \  /     \ 
+:: /    |    \/ /_/  >   Y  \  |/ __ \_ |    |___ / __ \|  Y Y  \
+:: \____|__  /\___  /|___|  /__(____  / |_______ (____  /__|_|  /
+::         \//_____/      \/        \/          \/    \/      \/ 
+:: --------------------------------------------------------------
+:: License: GNU GENERAL PUBLIC LICENSE Version 3 
+
+::    This program is free software: you can redistribute it and/or modify
+::    it under the terms of the GNU General Public License as published by
+::    the Free Software Foundation, either version 3 of the License, or
+::    (at your option) any later version.
+::
+::    This program is distributed in the hope that it will be useful,
+::    but WITHOUT ANY WARRANTY; without even the implied warranty of
+::    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+::    GNU General Public License for more details.
+::
+::    You should have received a copy of the GNU General Public License
+::    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+@echo off
+call bin\buildsuper_x64-win.bat 4coder_penguin\4coder_penguin.cpp
+copy custom_4coder.dll ..\custom_4coder.dll
