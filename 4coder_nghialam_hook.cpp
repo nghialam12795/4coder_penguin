4a1fbc275f80f8c85fe0c1315705b0acb5646291
Nghia Lam <nghialam12795@gmail.com>
2020-12-02 20:25:32 +0700

    Add file structures

--- 4coder_nghialam_hook.cpp
+++ 4coder_nghialam_hook.cpp
@@ -23,29 +23,14 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // ==========================================================================
 
-#include "4coder_default_include.cpp"
-#include "generated/managed_id_metadata.cpp"
+//~ Main APIs
+CUSTOM_COMMAND_SIG(nghialam_startup);             // Custom layer startup
 
-#include "4coder_nghialam/4coder_nghialam_base.cpp"
-#include "4coder_nghialam/4coder_nghialam_hook.cpp"
 
-//~ TODO(Nghia Lam): Create my custom layer
-//  - [ ] VI Model Editing System ?? 
-//  - [ ] Plot comment system
-//  - [ ] Stuff for rendering
-//  - [ ] My own file bar rendering
-//  - [ ] Project management
-//  - [ ] Project todo list
-
-//~ NOTE(Nghia Lam): Entry Point here
-void custom_layer_init(Application_Links *app) {
-  Thread_Context *tctx = get_thread_context(app);
+//~ Implementation
+CUSTOM_COMMAND_SIG(nghialam_startup)
+CUSTOM_DOC("NghiaLam's custom layer startup event") {
+  ProfileScope(app, "default startup");
   
-  // NOTE(Allen): setup for default framework
-  default_framework_init(app);
-  
-  // NOTE(Allen): default hooks and command maps
-  set_all_default_hooks(app);
-  mapping_init(tctx, &framework_mapping);
-  setup_default_mapping(&framework_mapping, mapid_global, mapid_file, mapid_code);
+  // Init startup code here
 }
