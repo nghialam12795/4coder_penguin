2119f26c7c806fc6ba285cc047a2c214695896fd
Nghia Lam <nghialam12795@gmail.com>
2020-12-03 16:53:03 +0700

    Render whole buffer hook

--- 4coder_nghialam_draw.cpp
+++ 4coder_nghialam_draw.cpp
@@ -29,6 +29,9 @@
 //~ NOTE(Nghia Lam): Main drawing API
 function Rect_f32 NL_DrawBackground(Application_Links* app, View_ID view, b32 is_active_view, String_Const_u8 buffer_name);
 function void NL_DrawFileBar(Application_Links *app, View_ID view_id, Buffer_ID buffer, Face_ID face_id, Rect_f32 bar);
+function void NL_DrawCppTokenColors(Application_Links *app, Text_Layout_ID text_layout_id, Token_Array *array);
+function void NL_DrawBraceHighlight(Application_Links *app, Buffer_ID buffer, Text_Layout_ID text_layout_id, i64 position, ARGB_Color *colors, i32 color_count);
+function void NL_DrawDividerComment(Application_Links *app, Buffer_ID buffer, View_ID view, Text_Layout_ID text_layout_id);
 
 //~ NOTE(Nghia Lam): My Implementation
 function Rect_f32 NL_DrawBackground(Application_Links* app, 
@@ -58,8 +61,110 @@ function void NL_DrawFileBar(Application_Links *app,
                              Buffer_ID buffer, 
                              Face_ID face_id, 
                              Rect_f32 bar) {
-  // TODO(Nghia Lam): Draw my own file bar, currently draw default file bar for now
+  // TODO(Nghia Lam): Draw my own file bar, currently draw default file bar for now.
   draw_file_bar(app, view_id, buffer, face_id, bar);
+}
+
+function void NL_DrawCppTokenColors(Application_Links *app,
+                                    Text_Layout_ID text_layout_id,
+                                    Token_Array *array) {
+  // TODO(Nghia Lam): Draw my own cpp token, currently draw default by now.
+  draw_cpp_token_colors(app, text_layout_id, array);
+}
+
+function void NL_DrawBraceHighlight(Application_Links *app,
+                                    Buffer_ID buffer,
+                                    Text_Layout_ID text_layout_id,
+                                    i64 position,
+                                    ARGB_Color *colors,
+                                    i32 color_count) {
+  ProfileScope(app, "[Nghia Lam] Brace Highlight");
+  
+  Token_Array token_array = get_token_array_from_buffer(app, buffer);
+  if (token_array.tokens != 0) {
+    Token_Iterator_Array it = token_iterator_pos(0, &token_array, position);
+    Token *token = token_it_read(&it);
+    if(token != 0 && token->kind == TokenBaseKind_ScopeOpen) {
+      position = token->pos + token->size;
+    } else {
+      
+      if(token_it_dec_all(&it)) {
+        token = token_it_read(&it);
+        
+        if (token->kind == TokenBaseKind_ScopeClose &&
+            position == token->pos + token->size) {
+          position = token->pos;
+        }
+      }
+    }
+  }
+  
+  draw_enclosures(app,
+                  text_layout_id, 
+                  buffer,
+                  position,
+                  FindNest_Scope,
+                  RangeHighlightKind_CharacterHighlight,
+                  0, 0, 
+                  colors, 
+                  color_count);
+}
+
+function void NL_DrawDividerComment(Application_Links *app,
+                                    Buffer_ID buffer,
+                                    View_ID view,
+                                    Text_Layout_ID text_layout_id) {
+  ProfileScope(app, "[Nghia Lam] Divider Comments");
+  
+  String_Const_u8 divider_comment_signifier = string_u8_litexpr("//~");
+  
+  Token_Array token_array = get_token_array_from_buffer(app, buffer);
+  Range_i64 visible_range = text_layout_get_visible_range(app, text_layout_id);
+  Scratch_Block scratch(app);
+  
+  if(token_array.tokens != 0) {
+    i64 first_index         = token_index_from_pos(&token_array, visible_range.first);
+    Token_Iterator_Array it = token_iterator_index(0, &token_array, first_index);
+    
+    Token *token = 0;
+    for (;;) {
+      token = token_it_read(&it);
+      if(token->pos >= visible_range.one_past_last 
+         || !token 
+         || !token_it_inc_non_whitespace(&it)) {
+        break;
+      }
+      
+      if(token->kind == TokenBaseKind_Comment) {
+        Rect_f32 comment_first_char_rect = text_layout_character_on_screen(app, 
+                                                                           text_layout_id, 
+                                                                           token->pos);
+        
+        Range_i64 token_range = {
+          token->pos,
+          token->pos + (token->size > (i64)divider_comment_signifier.size
+                        ? (i64)divider_comment_signifier.size
+                        : token->size),
+        };
+        
+        u8 token_buffer[256] = {0};
+        buffer_read_range(app, buffer, token_range, token_buffer);
+        String_Const_u8 token_string = { token_buffer, (u64)(token_range.end - token_range.start) };
+        
+        if(string_match(token_string, divider_comment_signifier)) {
+          // NOTE(rjf): Render divider line.
+          Rect_f32 rect = {
+            comment_first_char_rect.x0,
+            comment_first_char_rect.y0-2,
+            10000,
+            comment_first_char_rect.y0,
+          };
+          f32 roundness = 4.f;
+          draw_rectangle(app, rect, roundness, fcolor_resolve(fcolor_id(defcolor_comment)));
+        }
+      }
+    }
+  }
 }
 
 #endif // FCODER_NGHIALAM_DRAW
--- 4coder_nghialam_hook.cpp
+++ 4coder_nghialam_hook.cpp
@@ -131,7 +131,7 @@ function void NL_RenderCaller(Application_Links *app,
   
   // NOTE(rjf): Draw inactive rectangle
   if(is_active_view == 0) {
-    draw_rectangle(app, view_rect, 0.f, 0x44000000);
+    draw_rectangle(app, region, 0.f, 0x44000000);
   }
   
   // NOTE(Nghia Lam): Cleanup
@@ -141,7 +141,7 @@ function void NL_RenderCaller(Application_Links *app,
 
 function void NL_WholeScreenRenderCaller(Application_Links *app, 
                                          Frame_Info frame_info) {
-  
+  // TODO(Nghia Lam): Find a way to work with this, maybe render item list and todo list here.
 }
 
 
@@ -151,8 +151,135 @@ function void NL_RenderBuffer(Application_Links *app,
                               Buffer_ID buffer,
                               Text_Layout_ID text_layout_id,
                               Rect_f32 rect) {
-  // TODO(Nghia Lam): Custom drawing buffer, this is leave default for now
-  default_render_buffer(app, view_id, face_id, buffer, text_layout_id, rect);
+  ProfileScope(app, "[NghiaLam] Render Buffer");
+  
+  View_ID active_view = get_active_view(app, Access_Always);
+  b32 is_active_view  = (active_view == view_id);
+  Rect_f32 prev_clip  = draw_set_clip(app, rect);
+  
+  // NOTE(Nghia Lam): Token coloring
+  Token_Array token_array = get_token_array_from_buffer(app, buffer);
+  if (token_array.tokens != 0){
+    // TODO(Nghia Lam): Support other code type token.
+    NL_DrawCppTokenColors(app, text_layout_id, &token_array);
+    
+    // NOTE(Nghia Lam): Scan for TODOs, NOTEs and user name
+    if (global_config.use_comment_keyword){
+      char user_string_buf[256] = {0};
+      String_Const_u8 user_string = {0};
+      {
+        user_string.data = user_string_buf;
+        user_string.size = snprintf(user_string_buf, 
+                                    sizeof(user_string_buf), 
+                                    "(%.*s)",
+                                    string_expand(global_config.user_name));
+      }
+      Comment_Highlight_Pair pairs[] = {
+        {string_u8_litexpr("NOTE"), finalize_color(defcolor_comment_pop, 0)},
+        {string_u8_litexpr("TODO"), finalize_color(defcolor_comment_pop, 1)},
+        {user_string, 0xffffdd23}, // TODO(Nghia Lam): Use from config file for this color.
+      };
+      draw_comment_highlights(app, buffer, text_layout_id, &token_array, pairs, ArrayCount(pairs));
+    }
+  }
+  else{
+    Range_i64 visible_range = text_layout_get_visible_range(app, text_layout_id);
+    paint_text_color_fcolor(app, text_layout_id, visible_range, fcolor_id(defcolor_text_default));
+  }
+  
+  i64 cursor_pos = view_correct_cursor(app, view_id);
+  view_correct_mark(app, view_id);
+  
+  // NOTE(Allen): Scope highlight
+  if(global_config.use_scope_highlight) {
+    Color_Array colors = finalize_color_array(defcolor_back_cycle);
+    draw_scope_highlight(app, buffer, text_layout_id, cursor_pos, colors.vals, colors.count);
+  }
+  
+  // NOTE(Nghia Lam): Brace Highlight
+  // TODO(Nghia Lam): Use global config for this settings
+  {
+    ARGB_Color colors[] = {
+      0xff8ffff2,
+    };
+    NL_DrawBraceHighlight(app,
+                          buffer,
+                          text_layout_id,
+                          cursor_pos,
+                          colors,
+                          sizeof(colors)/sizeof(colors[0]));
+  }
+  
+  if (global_config.use_error_highlight || global_config.use_jump_highlight) {
+    // NOTE(Allen): Error highlight
+    String_Const_u8 name = string_u8_litexpr("*compilation*");
+    Buffer_ID compilation_buffer = get_buffer_by_name(app, name, Access_Always);
+    if (global_config.use_error_highlight) {
+      draw_jump_highlights(app,
+                           buffer,
+                           text_layout_id,
+                           compilation_buffer,
+                           fcolor_id(defcolor_highlight_junk));
+    }
+    
+    // NOTE(Allen): Search highlight
+    if (global_config.use_jump_highlight) {
+      Buffer_ID jump_buffer = get_locked_jump_buffer(app);
+      if (jump_buffer != compilation_buffer) {
+        draw_jump_highlights(app,
+                             buffer,
+                             text_layout_id,
+                             jump_buffer, 
+                             fcolor_id(defcolor_highlight_white));
+      }
+    }
+  }
+  
+  // NOTE(Allen): Line highlight
+  if (global_config.highlight_line_at_cursor && is_active_view){
+    i64 line_number = get_line_number_from_pos(app, buffer, cursor_pos);
+    draw_line_highlight(app,
+                        text_layout_id,
+                        line_number,
+                        fcolor_id(defcolor_highlight_cursor_line));
+  }
+  
+  // NOTE(Allen): Color parens
+  if (global_config.use_paren_helper) {
+    Color_Array colors = finalize_color_array(defcolor_text_cycle);
+    draw_paren_highlight(app, buffer, text_layout_id, cursor_pos, colors.vals, colors.count);
+  }
+  
+  // NOTE(Allen): Cursor
+  Face_Metrics metrics = get_face_metrics(app, face_id);
+  f32 cursor_roundness = metrics.normal_advance * global_config.cursor_roundness;
+  f32 mark_thickness   = (f32) global_config.mark_thickness;
+  
+  switch (fcoder_mode) {
+    case FCoderMode_Original: {
+      draw_original_4coder_style_cursor_mark_highlight(app, 
+                                                       view_id, 
+                                                       is_active_view, 
+                                                       buffer, 
+                                                       text_layout_id, 
+                                                       cursor_roundness, 
+                                                       mark_thickness);
+    } break;
+    case FCoderMode_NotepadLike: {
+      draw_notepad_style_cursor_highlight(app, view_id, buffer, text_layout_id, cursor_roundness);
+    } break;
+  }
+  
+  // NOTE(Nghia Lam): Divider Comments
+  NL_DrawDividerComment(app, buffer, view_id, text_layout_id);
+  
+  // NOTE(Allen): Fade ranges
+  paint_fade_ranges(app, text_layout_id, buffer);
+  
+  // NOTE(Allen): put the actual text on the actual screen
+  draw_text_layout_default(app, text_layout_id);
+  
+  draw_set_clip(app, prev_clip);
 }
 
 #endif // FCODER_NGHIALAM_HOOKS
